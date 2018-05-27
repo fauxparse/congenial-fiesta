7dacfe37024989aee4c4df2882f5d52679e05457
Matt Powell <fauxparse@gmail.com>
2018-05-27 18:46:02 +1200

    move oauth settings into secure credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -18,10 +18,16 @@ module OmniAuth
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'],
+  provider :google_oauth2,
+    Rails.application.credentials.google[:key],
+    Rails.application.credentials.google[:secret],
     name: :google
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :facebook,
+    Rails.application.credentials.facebook[:key],
+    Rails.application.credentials.facebook[:secret]
+  provider :twitter,
+    Rails.application.credentials.twitter[:key],
+    Rails.application.credentials.twitter[:secret]
   provider :developer unless Rails.env.production?
 end
 
