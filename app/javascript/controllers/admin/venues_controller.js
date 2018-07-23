0b52c9491d02f3075980db0ad5d4316e6b798c6e
Matt Powell <fauxparse@gmail.com>
2018-07-24 08:24:22 +1200

    properly stop checking for mapbox loading

--- app/javascript/controllers/admin/venues_controller.js
+++ app/javascript/controllers/admin/venues_controller.js
@@ -161,7 +161,7 @@ export default class extends Controller {
     new Promise(resolve => {
       const checker = setInterval(() => {
         if (window.mapboxgl && window.MapboxGeocoder) {
-          clearInterval(this.checker)
+          clearInterval(checker)
           resolve()
         }
       }, 100)
