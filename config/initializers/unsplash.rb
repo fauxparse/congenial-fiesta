fa29b2a82c8d0f603783646301bd7fcddddae7fa
Matt Powell <fauxparse@gmail.com>
2018-08-23 17:25:47 +1200

    fix unsplash config

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -2,6 +2,6 @@
 
 Unsplash.configure do |config|
   credentials = Rails.application.credentials.unsplash
-  config.application_id = credentials[:access_key]
+  config.application_access_key = credentials[:access_key]
   config.application_secret = credentials[:secret_key]
 end
