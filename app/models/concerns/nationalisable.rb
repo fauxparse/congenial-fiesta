6aca9e73c9d21245855f53beef085a5dad274a90
Matt Powell <fauxparse@gmail.com>
2018-09-03 10:00:45 +1200

    fix blank country error

--- app/models/concerns/nationalisable.rb
+++ app/models/concerns/nationalisable.rb
@@ -4,7 +4,7 @@ module Nationalisable
   extend ActiveSupport::Concern
 
   def country
-    return nil unless country_code
+    return nil if country_code.blank?
     country = ISO3166::Country[country_code]
     country.translations[I18n.locale.to_s] || country.name
   end
