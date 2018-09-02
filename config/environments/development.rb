835d095834cf07f0a928b6fb13e1f1ca4ace887d
Matt Powell <fauxparse@gmail.com>
2018-09-02 18:19:44 +1200

    default host settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options =
-    { protocol: 'https', host: 'localhost', port: 3000 }
+    { protocol: 'http', host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
+  config.action_mailer.default_url_options =
+    { protocol: 'https', host: 'nzif.info' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
