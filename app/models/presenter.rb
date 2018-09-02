265cb77d245f0f9616c3b024bbf9c89a00a68c40
Matt Powell <fauxparse@gmail.com>
2018-09-03 08:53:57 +1200

    cope with blank country

--- app/models/presenter.rb
+++ app/models/presenter.rb
@@ -12,15 +12,22 @@ class Presenter < ApplicationRecord
   end
 
   def location
-    @location ||=
-      if participant.country_code == 'nz'
-        participant.city || 'NZ'
-      else
-        I18n.t(
-          participant.country_code&.downcase,
-          scope: :countries,
-          default: participant.country
-        )
-      end
+    @location ||= abbreviated_location(participant.country_code)
+  end
+
+  private
+
+  def abbreviated_location(code)
+    if code.blank?
+      ''
+    elsif code == 'nz'
+      participant.city || 'NZ'
+    else
+      I18n.t(
+        participant.country_code&.downcase,
+        scope: :countries,
+        default: participant.country
+      )
+    end
   end
 end
