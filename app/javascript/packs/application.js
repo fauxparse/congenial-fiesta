f891396e47a7bfec3f0fb725157066e7b1407e60
Matt Powell <fauxparse@gmail.com>
2018-09-04 09:53:33 +1200

    google analytics

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ ActiveStorage.start()
 const application = Application.start()
 const context = require.context('../controllers', false, /\.js$/)
 application.load(definitionsFromContext(context))
+
+document.addEventListener('turbolinks:load', (event) => {
+  if (window.ga && typeof(ga) === 'function') {
+    ga('set', 'location', event.data.url)
+    ga('send', 'pageview')
+  }
+})
