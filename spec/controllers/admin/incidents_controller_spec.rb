5f6a8a14c6965aabf6952167392534efa4e69294
Matt Powell <fauxparse@gmail.com>
2018-10-19 13:03:38 +1300

    fix incident controller spec

--- spec/controllers/admin/incidents_controller_spec.rb
+++ spec/controllers/admin/incidents_controller_spec.rb
@@ -1,19 +1,26 @@
+# frozen_srtring_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Admin::IncidentsController, type: :controller do
+RSpec.describe Admin::IncidentsController, type: :request do
+  let(:festival) { create(:festival) }
+  let(:admin) { create(:admin) }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  before { log_in_as(admin) }
+
+  describe 'GET /admin/:year/incidents' do
+    it 'is successful' do
+      get admin_incidents_path(festival)
+      expect(response).to be_successful
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  describe 'GET /admin/:year/incidents/:id' do
+    let(:incident) { create(:incident, festival: festival) }
+
+    it 'is successful' do
+      get admin_incident_path(festival, incident)
+      expect(response).to be_successful
     end
   end
-
 end
